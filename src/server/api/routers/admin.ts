72331326cacb81af0c62cf3848a7d3bf16e775dc
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-01-20 20:06:14 -0300

    implement add item and delete item in back end

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { adminProcedure, createTRPCRouter, publicProcedure } from "../trpc";
 import { z } from "zod";
 import { SignJWT } from "jose";
@@ -51,12 +56,62 @@ export const adminRouter = createTRPCRouter({
       const ex = input.fileType.split("/")[1];
       const key = `${id}.${ex}`;
 
-      const { url, fields } = await new Promise((resolve, reject) => {
-        s3.createPresignedPost({
-          Bucket: "restaurant-booking-system2",
-          Fields: { key },
-          Conditions: [["content-length-range", 0, MAX_FILE_SIZE]],
-        });
+      const { url, fields } = (await new Promise((resolve, reject) => {
+        s3.createPresignedPost(
+          {
+            Bucket: "restaurant-booking-system2",
+            Fields: { key },
+            Conditions: [
+              ["content-length-range", 0, MAX_FILE_SIZE],
+              ["starts-with", "$Content-Type", "image/"],
+            ],
+          },
+
+          (err, data) => {
+            if (err) return reject(err);
+            resolve(data);
+          },
+        );
+      })) as any as { url: string; fields: any };
+
+      return { url, fields, key };
+    }),
+
+  addMenuItem: adminProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        price: z.number(),
+        imageKey: z.string(),
+        categories: z.array(
+          z.union([
+            z.literal("breakfast"),
+            z.literal("launch"),
+            z.literal("dinner"),
+          ]),
+        ),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { name, price, categories, imageKey } = input;
+      const menuItem = await ctx.db.menuItem.create({
+        data: { name, price, categories, imageKey },
       });
+
+      return menuItem;
+    }),
+
+  deleteMenuItem: adminProcedure
+    .input(z.object({ imageKey: z.string(), id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { id, imageKey } = input;
+
+      await s3
+        .deleteObject({ Bucket: "restaurant-booking-system2", Key: imageKey })
+        .promise();
+
+      const menuItem = await ctx.db.menuItem.delete({ where: { id } });
+
+      return menuItem;
     }),
 });
