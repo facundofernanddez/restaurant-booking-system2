645f5224c7cde4a1c4124c25b498cdcf36d97710
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-01-23 09:15:41 -0300

    get menu items and check

--- src/server/api/routers/menu.ts
+++ src/server/api/routers/menu.ts
@@ -1,3 +1,29 @@
-import { createTRPCRouter } from "../trpc";
+import { s3 } from "@/lib/s3";
+import { createTRPCRouter, publicProcedure } from "../trpc";
 
-export const menuRouter = createTRPCRouter({});
+export const sleep = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+export const menuRouter = createTRPCRouter({
+  getMenuItems: publicProcedure.query(async ({ ctx }) => {
+    const menuItems = await ctx.db.menuItem.findMany();
+
+    const withUrls = await Promise.all(
+      menuItems.map(async (menuItem) => ({
+        ...menuItems,
+        url: await s3.getSignedUrlPromise("getObject", {
+          Bucket: "restaurant-booking-system2",
+          Key: menuItem.imageKey,
+        }),
+      })),
+    );
+
+    return withUrls;
+  }),
+
+  checkMenuStatus: publicProcedure.mutation(async () => {
+    await sleep(1000);
+
+    return { success: true };
+  }),
+});
