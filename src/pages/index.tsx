9696c652839d934f3426859d23351e26aa3da29c
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-01-24 09:07:08 -0300

    check menu status

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
 import Calendar from "@/components/Calendar";
 import Menu from "@/components/Menu";
 import Spinner from "@/components/Spinner";
+import { api } from "@/utils/api";
 import type { DateType } from "@/utils/types";
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [date, setDate] = useState<DateType>({
     justDate: null,
     dateTime: null,
   });
+
+  useEffect(() => {
+    if (date.dateTime) checkMenuStatus();
+  }, [date]);
+
+  const { mutate: checkMenuStatus, isSuccess } =
+    api.menu.checkMenuStatus.useMutation();
 
   return (
     <>
@@ -20,7 +28,7 @@ export default function Home() {
 
       <main>
         {!date.dateTime && <Calendar setDate={setDate} date={date} />}
-        {date.dateTime && false ? (
+        {date.dateTime && isSuccess ? (
           <Menu />
         ) : (
           <div className="flex h-screen items-center justify-center">
