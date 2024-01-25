e0135de4128bc87b02564158c4bab804ed1006c8
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-01-25 09:49:35 -0300

    create helper function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -6,3 +6,21 @@ export const selectOptions = categories.map((ctg) => ({
   value: ctg,
   label: capitalize(ctg),
 }));
+
+export const weekdayIndexToName = (index: number) => {
+  const days = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+  ];
+
+  return days[index];
+};
+
+export function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
