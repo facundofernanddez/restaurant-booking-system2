52194381a548bde23440452d9a1f3db4b743db46
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-01-24 23:26:26 -0300

    export the function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,6 @@
 import { categories } from "@/constants/config";
 
-const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
+export const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 
 export const selectOptions = categories.map((ctg) => ({
   value: ctg,
