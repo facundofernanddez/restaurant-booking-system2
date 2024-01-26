e0f055c2614b816bd033ab043ad6d93e4791cf1b
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-01-26 09:35:45 -0300

    create more helper functions

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,6 @@
 import { categories } from "@/constants/config";
+import type { Day } from "@prisma/client";
+import { addMinutes, getMinutes, isEqual, parse } from "date-fns";
 
 export const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 
@@ -24,3 +26,29 @@ export const weekdayIndexToName = (index: number) => {
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
+
+export const roundToNearestMinutes = (date: Date, interval: number) => {
+  const minutesLeftUntilNextInterval = interval - (getMinutes(date) % interval);
+
+  return addMinutes(date, minutesLeftUntilNextInterval);
+};
+
+export const getOpeningTimes = (startDate: Date, dbDays: Day[]) => {
+  const dayOfWeek = startDate.getDay();
+  const isToday = isEqual(
+    startDate,
+    new Date("January 15, 2023, 12:00:00").setHours(0, 0, 0, 0),
+  );
+
+  const today = dbDays.find((d) => d.dayOfWeek === dayOfWeek);
+  if (!today) throw new Error("This day does not exist in db");
+
+  const opening = parse(today.openTime, "kk:mm", startDate);
+  const closing = parse(today.closeTime, "kk:mm", startDate);
+
+  let hours: number;
+  let minutes: number;
+
+  if (isToday) {
+  }
+};
