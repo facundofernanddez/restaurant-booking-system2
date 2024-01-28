83cb714e5ce337cce463deb202d556dc942e3ac0
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-01-27 21:11:11 -0300

    add now constant

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -5,3 +5,5 @@ export const INTERVAL = 30; // in minutes
 export const categories = ["all", "breakfast", "lunch", "dinner"] as const;
 
 export const MAX_FILE_SIZE = 1024 * 1024 * 5; // 5MB
+
+export const now = new Date();
