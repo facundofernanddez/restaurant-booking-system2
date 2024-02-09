a4a64f9c19e45b91afe48cf7e2ac2cdeb82c0c55
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-02-09 10:17:25 -0300

    create listbox

--- src/components/TimeSelector.tsx
+++ src/components/TimeSelector.tsx
@@ -1,5 +1,7 @@
 import { classNames } from "@/utils/helpers";
-import { Listbox } from "@headlessui/react";
+import { Listbox, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import { HiSelector } from "react-icons/hi";
 
 interface TimeSelectorProps {
   changeTime: (time: string, type: "openTime" | "closeTime") => void;
@@ -50,7 +52,36 @@ export default function TimeSelector({
                 />
                 <span className="ml-2 block truncate">{selected}</span>
               </div>
+              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                <HiSelector
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </span>
             </Listbox.Button>
+
+            <Transition
+              show={open}
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {timeOptions.map((time) => (
+                  <Listbox.Option
+                    key={time}
+                    className={({ active }) =>
+                      classNames(
+                        active ? "bg-indigo-600 text-white" : "text-gray-900",
+                        "relative cursor-default select-none py-2 pl-3 pr-9",
+                      )
+                    }
+                    value={time}
+                  ></Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
           </div>
         </>
       )}
