efa3e0e70c4761b37f34cf87729363d22f42af2a
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-02-09 08:58:04 -0300

    Update TimeSelector.tsx

--- src/components/TimeSelector.tsx
+++ src/components/TimeSelector.tsx
@@ -1,3 +1,4 @@
+import { classNames } from "@/utils/helpers";
 import { Listbox } from "@headlessui/react";
 
 interface TimeSelectorProps {
@@ -31,6 +32,28 @@ export default function TimeSelector({
 
         changeTime(e, type);
       }}
-    ></Listbox>
+    >
+      {({ open }) => (
+        <>
+          <Listbox.Label className="block w-32 text-sm font-medium text-gray-700">
+            {type === "openTime" ? "Opening time" : "Closing time"}
+          </Listbox.Label>
+          <div className="relative mt-1">
+            <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
+              <div className="flex items-center">
+                <span
+                  aria-label={true ? "Online" : "Offline"}
+                  className={classNames(
+                    true ? "bg-green-400" : "bg-gray-200",
+                    "inline-block h-2 w-2 flex-shrink-0 rounded-full",
+                  )}
+                />
+                <span className="ml-2 block truncate">{selected}</span>
+              </div>
+            </Listbox.Button>
+          </div>
+        </>
+      )}
+    </Listbox>
   );
 }
