0ff8ee6a1526ace7c9aeb408cff29ddb308ad717
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-02-17 10:47:08 -0300

    create functions and pass to Menu component

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,3 +1,4 @@
+import Cart from "@/components/Cart";
 import Menu from "@/components/Menu";
 import Spinner from "@/components/Spinner";
 import { now } from "@/constants/config";
@@ -5,6 +6,7 @@ import { api } from "@/utils/api";
 import { parseISO } from "date-fns";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { BsCart } from "react-icons/bs";
 
 export default function MenuPage() {
   const router = useRouter();
@@ -13,6 +15,37 @@ export default function MenuPage() {
   const { isFetchedAfterMount } = api.menu.checkMenuStatus.useQuery(undefined, {
     onError: () => {},
   });
+
+  const { mutate } = api.checkout.checkoutSession.useMutation({
+    onSuccess: ({ url }) => {
+      router.push(url);
+    },
+  });
+
+  const [showCart, setShowCart] = useState<boolean>(false);
+  const [productsInCart, setProductsInCart] = useState<
+    { id: string; quantity: number }[]
+  >([]);
+
+  const addToCart = (id: string, quantity: number) => {
+    setProductsInCart((prev) => {
+      const existing = prev.find((item) => item.id === id);
+
+      if (existing) {
+        return prev.map((item) => {
+          if (item.id === id)
+            return { ...item, quantity: item.quantity + quantity };
+          return item;
+        });
+      }
+
+      return [...prev, { id, quantity }];
+    });
+  };
+
+  const removeFromCart = (id: string) => {
+    setProductsInCart((prev) => prev.filter((item) => item.id !== id));
+  };
 
   useEffect(() => {
     const selectedTime = localStorage.getItem("selectedTime");
@@ -27,12 +60,26 @@ export default function MenuPage() {
 
   return (
     <>
+      <Cart
+        removeFromCart={removeFromCart}
+        open={showCart}
+        setOpen={setShowCart}
+        products={productsInCart}
+      />
       {isFetchedAfterMount && selectedTime ? (
         <div className="mx-auto mt-12 max-w-7xl sm:px-6 lg:px-8">
-          <button onClick={() => router.push("/")}>
-            Back to time selection
-          </button>
-          <Menu selectedTime={selectedTime} />
+          <div className="flex w-full justify-end">
+            <button
+              type="button"
+              className="flex items-center justify-center rounded-lg bg-gray-200 p-3 text-lg font-medium text-indigo-600"
+              onClick={() => setShowCart((prev) => !prev)}
+            >
+              <BsCart className="mr-2 text-lg" />
+              {productsInCart.reduce((acc, item) => acc + item.quantity, 0)}
+            </button>
+          </div>
+
+          <Menu addToCart={addToCart} selectedTime={selectedTime} />
         </div>
       ) : (
         <>
