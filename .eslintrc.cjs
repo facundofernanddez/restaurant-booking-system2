05e230ca5f73342ec1bc84574c1dd79e71c88cc7
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-02-18 20:01:38 -0300

    add global linter config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,10 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
 
+    //add from myself
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
